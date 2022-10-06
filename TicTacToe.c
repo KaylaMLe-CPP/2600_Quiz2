3e33c0f956de98d644df7f702edf81259b32cd50
Kayla Marie Le <kaylale2014@gmail.com>
2022-10-05 19:12:34 -0700

    outlined program in comments

---
+++ TicTacToe.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main()
+{
+    // prompt for play mode
+    //// display game board
+
+    ////// ask p1 for x coord
+    ////// ask p1 for y coord
+    ////// if not empty, reprompt
+
+    //// check win condition
+
+    ////// ask p2/randomize x ccord
+    ////// ask p2/randomize y coord
+    ////// if not empty, reprompt
+
+    //// check win condition
+}
