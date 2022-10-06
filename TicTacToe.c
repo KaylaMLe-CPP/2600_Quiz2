8f1aecde755133cd5d00d51aec2f37247b5de7cb
Kayla Marie Le <kaylale2014@gmail.com>
2022-10-06 01:24:21 -0700

    figured out stdin for c

--- TicTacToe.c
+++ TicTacToe.c
@@ -1,19 +1,36 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdbool.h>
+#include <time.h>
+
+void printBoard(char board[10])
+{
+    printf("\n   0 1 2\n   - - -\n");
+
+    for (int y = 0; y < 3; y++)
+    {
+        printf("%d ", y);
+        for (int x = 0; x < 3; x++)
+        {
+            printf("|%c", board[y * 3 + x]);
+        }
+
+        printf("|\n   - - -\n");
+    }
+}
 
 int main()
 {
     // prompt for play mode
-    //// display game board
-
-    ////// ask p1 for x coord
-    ////// ask p1 for y coord
-    ////// if not empty, reprompt
-
-    //// check win condition
-
-    ////// ask p2/randomize x ccord
-    ////// ask p2/randomize y coord
-    ////// if not empty, reprompt
-
-    //// check win condition
+    char playMode[2];
+    int players;
+    printf("Enter\n\t1\tif you want to play against a bot\n\t2\tif you want to play against another person\n");
+    do
+    {
+        gets(playMode);
+        players = atoi(playMode);
+    } while (players != 1 && players != 2);
+    // display game board
+    char board[10] = "         ";
 }
